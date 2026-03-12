00b7e4b6f52242e172fc4b4e6bc66242973dfb1d
happyladysauce <13452552349@163.com>
2026-03-12 09:53:45 +0800

    feat: 添加二分插入排序实现及相关测试

--- 数据结构与算法/排序算法/golang/插入排序/main.go
+++ 数据结构与算法/排序算法/golang/插入排序/main.go
@@ -17,10 +17,38 @@ func insertionSort(arr []int) {
 	}
 }
 
+// 二分插入排序
+func binaryInsertSort(arr []int) {
+	for i := 1; i < len(arr); i++ {
+		key = arr[i]
+
+		left, right := 0, i - 1
+		for left <= right {
+			mid := left + (right - left) / 2
+			if arr[mid] > key {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+
+		for k := i; j > left; j-- {
+			arr[j] = arr[j - 1]
+		}
+
+		arr[left] = key
+	}
+
+
+} 
 
 func main() {
 	arr := []int{1, 4, 3, 5, 6, 7, 5, 832, 4, 523}
 	fmt.Println("排序前", arr)
 	insertionSort(arr)
+	fmt.Println("排序后", arr)
+	arr := []int{1, 4, 3, 5, 6, 7, 5, 832, 4, 523}
+	fmt.Println("排序前", arr)
+	binaryInsertSort(arr)
 	fmt.Println("排序后", arr)
 }
