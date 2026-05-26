4db3f173875b17e58ec43c538c8a79a6d356f5cc
happyladysauce <13452552349@163.com>
2026-05-26 16:16:10 +0800

    feat: 优化RabbitMQ初始化，添加通道和交换声明

--- Golang/RabbitMQ/mq/mq.go
+++ Golang/RabbitMQ/mq/mq.go
@@ -6,18 +6,24 @@ import (
 )
 
 var conn *amqp.Connection
-var ch *amqp.Channel
 
 func Init(url string) {
+	// create TCP connction for rabbitmq
 	conn, err := amqp.Dial(url)
 	if err != nil {
 		log.Fatalf("amqp connction error:", err)
 	}
 
+	// create channel to declare exchange
 	ch, err := conn.Channel()
 	if err != nil {
 		log.Fatalf("amqp Channel error:", err)
 	}
 
-	
+	// create "chat.direct" exchange
+	if err := ch.ExchangeDeclare(
+		"chat.direct", "direct", true, false, false, false, nil,
+	); err != nil {
+		log.Fatalf("amqp exchangDeclare error:", err)
+	}
 }
