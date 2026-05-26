e4aaa973798073caed6aeb918502b0950e0c7394
happyladysauce <13452552349@163.com>
2026-05-26 16:16:42 +0800

    fix: 修复日志记录中的错误处理方式

--- Golang/RabbitMQ/mq/mq.go
+++ Golang/RabbitMQ/mq/mq.go
@@ -11,19 +11,19 @@ func Init(url string) {
 	// create TCP connction for rabbitmq
 	conn, err := amqp.Dial(url)
 	if err != nil {
-		log.Fatalf("amqp connction error:", err)
+		log.Fatal("amqp connction error:", err)
 	}
 
 	// create channel to declare exchange
 	ch, err := conn.Channel()
 	if err != nil {
-		log.Fatalf("amqp Channel error:", err)
+		log.Fatal("amqp Channel error:", err)
 	}
 
 	// create "chat.direct" exchange
 	if err := ch.ExchangeDeclare(
 		"chat.direct", "direct", true, false, false, false, nil,
 	); err != nil {
-		log.Fatalf("amqp exchangDeclare error:", err)
+		log.Fatal("amqp exchangDeclare error:", err)
 	}
 }
